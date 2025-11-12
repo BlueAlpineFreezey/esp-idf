2c0a12286dc297920bb8b636307c1c5ae03c138e
New-Dawn-Rocketry <150757169+New-Dawn-Rocketry@users.noreply.github.com>
2025-11-12 11:26:37 -0700

    Modified spin lock file to remove warnings of a deprecated action.
    Functionally this is the same. We are just incrementing or decrementing the lock count. In C++ there is a warning that the official increment and decrement operators are not guaranteed to be atomic operations.

--- components/esp_hw_support/include/spinlock.h
+++ components/esp_hw_support/include/spinlock.h
@@ -106,7 +106,7 @@ static inline bool __attribute__((always_inline)) spinlock_acquire(spinlock_t *l
     // The caller is already the owner of the lock. Simply increment the nesting count
     if (lock->owner == core_owner_id) {
         assert(lock->count > 0 && lock->count < 0xFF);    // Bad count value implies memory corruption
-        lock->count++;
+        lock->count += 1;
 #if __XTENSA__
         XTOS_RESTORE_INTLEVEL(irq_status);
 #else
@@ -142,7 +142,7 @@ exit:
     if (lock_set) {
         assert(lock->owner == core_owner_id);
         assert(lock->count == 0);   // This is the first time the lock is set, so count should still be 0
-        lock->count++;  // Finally, we increment the lock count
+        lock->count += 1;  // Finally, we increment the lock count
     } else {    // We timed out waiting for lock
         assert(lock->owner == SPINLOCK_FREE || lock->owner == other_core_owner_id);
         assert(lock->count < 0xFF); // Bad count value implies memory corruption
@@ -190,7 +190,7 @@ static inline void __attribute__((always_inline)) spinlock_release(spinlock_t *l
     core_owner_id = rv_utils_get_core_id() == 0 ? SPINLOCK_OWNER_ID_0 : SPINLOCK_OWNER_ID_1;
 #endif
     assert(core_owner_id == lock->owner); // This is a lock that we didn't acquire, or the lock is corrupt
-    lock->count--;
+    lock->count -= 1;
 
     if (!lock->count) { // If this is the last recursive release of the lock, mark the lock as free
         lock->owner = SPINLOCK_FREE;
